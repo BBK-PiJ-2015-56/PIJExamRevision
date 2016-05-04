66d39fdd120d85a04ecb791a36459b3904e0e02e
Pete <pdomok01@mail.bbk.ac.uk>
2016-05-04 17:11:25 +0100

    changed class file structure - shouldn't affect what is pushed

--- java8stuff/LambdaPractice.java
+++ java8stuff/LambdaPractice.java
@@ -1,4 +1,4 @@
-//package pij.java8stuff;
+package java8stuff;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -116,11 +116,11 @@ public class LambdaPractice{
 	/*
 	3. Create a class with a static method called betterString. This method should take two Strings and a lambda as its arguments. This lambda states whether the first of the two strings is better. The method should return the better string; i.e., if the lambda returns true the method should return the first string, otherwise it should return the second string. Forthelambda,defineaninterfacecalledTwoStringPredicatewithamethodthat takes two Strings and returns true if the first is better than the second, false otherwise. Here are two examples: • returns whichever of test1 and test2 is longer, StringUtils.betterString(test1, test2, (s1, s2) -> s1.length() > s2.length()) • always returns test1, StringUtils.betterString(test1, test2, (s1, s2) -> true)
 	*/
-	public void q3{
+	public void q3(){
 
 	}
 	public static String betterString(String str1, String str2){
-		
+		return " ";
 	}
 
 }
--- java8stuff/TwoStringPredicate.java
+++ java8stuff/TwoStringPredicate.java
@@ -1,3 +1,5 @@
+package java8stuff;
+
 public interface TwoStringPredicate{
 	public String firstStringBetter(String str1, String str2);
 }
