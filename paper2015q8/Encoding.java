885d182d483e3e35f222dce5d1953ff22a628fe2
pdomok01 <pdomok01@mail.bbk.ac.uk>
2016-05-13 23:48:36 +0100

    finished 2015q8 - learnt how to convert String to List<Character>, and diff between an IntStream and a Stream<Integer>

--- paper2015q8/Encoding.java
+++ paper2015q8/Encoding.java
@@ -14,24 +14,35 @@ public class Encoding{
 	}
 	
 	public static void encode(String source, String from, String to){
-		//List<Character> chars = new ArrayList<Character>(source.toCharArray());
 		List<Character> fromList = from.chars().mapToObj(e->(char)e).collect(Collectors.toList());
 		List<Character> toList = to.chars().mapToObj(e->(char)e).collect(Collectors.toList());
+		// HOW COME IT IS A CHAR AND NOT AN OBJ????it is supposed to be an object valued stream
+		//ANS)  by object-based stream, they mean an implementation of Stream<> as opposed to 
+		// an IntStream - so mapToObj converts an IntStream into a Stream<> eg here into a Stream<Char>
+		
 		Map<Character,Character> fromTo = new HashMap<>();
 		// need to work out how to do thgis kinda thing without old for loops
 		// also stuff like reverse() method
 		for(int i = 0; i < fromList.size(); i++){
 			fromTo.put(fromList.get(i), toList.get(i));
 		}
+		//convert source string into list of characters
+		List<Character> sourceList = source.chars().mapToObj(e->(char)e).collect(Collectors.toList());
 		
-		List<Character> newList = source.chars().mapToObj(e->(char)e)
-												.map(c -> {
-													if(from.contains(c)) return fromTo.get(c);
-													else                 return c;
-												})
-												.collect(Collectors.toList());
-		System.out.println(newList);
+		//make the new string
+		//MUST LEARN HOW TO DO WITHOUT OLD FOR LOOP		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i< sourceList.size(); i++){
+			char next = sourceList.get(i);
+			if(fromList.contains(next))
+				sb.append(fromTo.get(next));
+			else
+				sb.append(next);	
+		}	
+		String result = sb.toString();
+		System.out.println(result);
 	}
+
 }
 
 	
